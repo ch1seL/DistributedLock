195c44a85e98ee3d682a72f32a112e342f6dec92
Alexander Salamatov <ch1seL@ya.ru>
2020-10-26 15:26:36 +0300

    ch1seL.DistributedLock.Abstraction version 0.0.3

---
+++ src/ch1seL.DistributedLock.Abstraction/DistributedLockBadStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Extensions.Caching.Distributed
+{
+    /// <summary>
+    ///     reused RedLockNet.RedLockStatus enum
+    /// </summary>
+    public enum DistributedLockBadStatus
+    {
+        Unlocked,
+        NoQuorum,
+        Conflicted,
+        Expired
+    }
+}
--- src/ch1seL.DistributedLock.Abstraction/DistributedLockException.cs
+++ src/ch1seL.DistributedLock.Abstraction/DistributedLockException.cs
@@ -4,11 +4,20 @@ namespace Microsoft.Extensions.Caching.Distributed
 {
     public class DistributedLockException : Exception
     {
-        public DistributedLockException(string resource, string lockId, string status) : base("Lock is not acquired")
+        public DistributedLockException(string resource, string lockId, DistributedLockBadStatus status) : base(
+            "Lock is not acquired")
         {
+            Resource = resource;
+            LockId = lockId;
+            Status = status;
+
             base.Data.Add("Resource", resource);
             base.Data.Add("LockId", lockId);
             base.Data.Add("Status", status);
         }
+
+        private string Resource { get; }
+        private string LockId { get; }
+        private DistributedLockBadStatus Status { get; }
     }
 }
